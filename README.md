843175e24030eba5fbe0ff49d92e274a4ae474e4
drdung1999 <duongdung12a8@gmail.com>
2019-12-31 06:21:55 +0700

    [1.0.0] add static page

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Awesome_chat
 Here I Learning how to create a a chat real time with NODE.JS
+
+# Awesome-Chat-Template-Frontend
+
+Trong Template ứng dụng  này có sử dụng những thư viện phía client với chính xác các phiên bản như sau:
+
+- bootstrap: **^3.3.7**
+- font-awesome **^4.7.0**
+- jquery **^3.3.1**
+- AlertifyJS **^1.11.0**
+- jquery.nicescroll **^3.7.6**
+- moment **^2.21.0**
+- emojionearea **^3.0.0**
+- peerjs **^0.3.14**
+- sweetalert2 **^7.33.1**
+- photoset-grid **^1.0.1**
+- jquery-colorbox **^1.6.4**
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import connectDB from './config/connectDB';
-import contactModel from './models/contact.model';
+import conFigViewEngine from './config/viewEngine';
 
 
 let app = express();
@@ -9,21 +9,18 @@ let app = express();
 // connect to mongodb
 connectDB();
 
+// config view engine
+conFigViewEngine(app);
 
-app.get('/',async function(req,res){
-  try {
-    let item = {
-      userId : 'duong hoang dung',
-      contactId : 'duong hoang dung',
-    }
-    let contact = await contactModel.createNew(item);
-    res.send(contact);
-  } catch (error) {
-    console.log(error);
-  }
+app.get('/',function(req,res){
+  return res.render('main/master');
+});
+
+app.get('/login-register',function(req,res){
+  return res.render('auth/loginRegister');
 });
 
 app.listen(process.env.APP_port,process.env.APP_host,function(){
   console.log('khoi chay thanh cong');
-});
+}); 
 
