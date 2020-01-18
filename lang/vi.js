688fbc0982e095ed0617a7cb569fcada896df619
drdung1999 <duongdung12a8@gmail.com>
2020-01-18 11:31:17 +0700

    [1.0.0] validator register part 1

---
+++ lang/vi.js
@@ -0,0 +1,6 @@
+export const transValidator = {
+  email_incorrect : "email phai co dang duongdung12a8@gmail.com",
+  gender_incorrect : "ua tai sao gioi tinh lai sai ??",
+  password_incorrect : "mat khau phai chua it nhat 8 ky tu bao gom chua hoa chu thuong va chu so",
+  password_confirmation_incorrect : "nhap lai mat khau chua chinh xac",
+};
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,26 @@
+import {validationResult} from 'express-validator/check';
 
 let loginRegister = function(req,res){
   return res.render('auth/master');
 }
 
-module.exports = loginRegister;
+let postRegister = (req,res) =>{
+  let errorsArr = [];
+  if(!validationResult(req).isEmpty()){
+   let validatorError = Object.values(validationResult(req).mapped());
+
+    validatorError.forEach(validatorError => {
+      errorsArr.push(validatorError.msg);
+    });
+    
+    console.log(errorsArr);
+    return;
+  }
+  console.log(req.body);
+
+}
+
+module.exports = {
+  loginRegister : loginRegister,
+  postRegister : postRegister
+}
