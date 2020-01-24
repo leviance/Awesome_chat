88cbca9d753bfdd4db615d4e9c2cfcd8392cf10b
drdung1999 <duongdung12a8@gmail.com>
2020-01-24 12:24:20 +0700

    [1.0.0] finish logout

--- src/routes/web.js
+++ src/routes/web.js
@@ -9,18 +9,18 @@ initPassportLocal();
 let router = express.Router();
 
 let initRoutes = (app) =>{
-  router.get('/',home);
-  
-  router.get('/login-register',auth.loginRegister);
-  router.post('/register',validator.register,auth.postRegister);
-  router.get('/verify/:token',auth.verifyAccount);
-
-  router.post('/login', passport.authenticate('local',{
+  router.get('/login-register',auth.checkLoggedOut ,auth.loginRegister);
+  router.post('/register',auth.checkLoggedOut, validator.register,auth.postRegister);
+  router.get('/verify/:token',auth.checkLoggedOut, auth.verifyAccount);
+  router.post('/login',auth.checkLoggedOut, passport.authenticate('local',{
     successRedirect: "/",
     failureRedirect: "/login-register",
     successFlash: true,
     failureFlash: true
   }));
+
+  router.get('/',auth.checkLoggedIn ,home);
+  router.get('/logout',auth.checkLoggedIn ,auth.getLogout)
 
   return app.use('/',router);
 
