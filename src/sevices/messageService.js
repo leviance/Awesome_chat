e329a5ac7fa11170070342ea76440df9fb32da83
drdung1999 <duongdung12a8@gmail.com>
2020-02-07 15:59:20 +0700

    [1.0.0] fix bug sort conversation

--- src/sevices/messageService.js
+++ src/sevices/messageService.js
@@ -12,11 +12,11 @@ let getAllConversationItems = (currentUserId) =>{
       let userConversationsPromise = contacts.map( async (contact) =>{
         if(contact.contactId == currentUserId){
           let getUserContact = await UserModel.getNormalUserDataById(contact.userId);
-          getUserContact.createdAt = contact.createdAt;
+          getUserContact.updatedAt = contact.updatedAt;
           return getUserContact;
         } else {
           let getUserContact = await UserModel.getNormalUserDataById(contact.contactId);
-          getUserContact.createdAt = contact.createdAt;
+          getUserContact.updatedAt = contact.updatedAt;
           return getUserContact;
         }        
       });
@@ -39,7 +39,7 @@ let getAllConversationItems = (currentUserId) =>{
       let allConversations = userConversations.concat(groupConversations);
 
       allConversations = _.sortBy(allConversations, (item) =>{ 
-        return -item.createdAt;
+        return -item.updatedAt;
       });
 
       resolve({
