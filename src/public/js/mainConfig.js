d02ead94b2ac51d2f8b43ac0ebb74e310febf1cb
drdung1999 <duongdung12a8@gmail.com>
2020-02-11 21:01:56 +0700

    [1.0.0] Filter message image & file

--- src/public/js/mainConfig.js
+++ src/public/js/mainConfig.js
@@ -10,15 +10,15 @@ function nineScrollLeft() {
   });
 }
 
-function nineScrollRight() {
-  $('.right .chat').niceScroll({
+function nineScrollRight(divId) {
+  $(`.right .chat[data-chat = ${divId}]`).niceScroll({
     smoothscroll: true,
     horizrailenabled: false,
     cursorcolor: '#ECECEC',
     cursorwidth: '7px',
     scrollspeed: 50
   });
-  $('.right .chat').scrollTop($('.right .chat')[0].scrollHeight);
+  $(`.right .chat[data-chat = ${divId}]`).scrollTop($(`.right .chat[data-chat = ${divId}]`)[0].scrollHeight);
 }
 
 function enableEmojioneArea(chatId) {
@@ -81,24 +81,29 @@ function configNotification() {
 }
 
 function gridPhotos(layoutNumber) {
-  let countRows = Math.ceil($('#imagesModal').find('div.all-images>img').length / layoutNumber);
-  let layoutStr = new Array(countRows).fill(layoutNumber).join("");
-  $('#imagesModal').find('div.all-images').photosetGrid({
-    highresLinks: true,
-    rel: 'withhearts-gallery',
-    gutter: '2px',
-    layout: layoutStr,
-    onComplete: function() {
-      $('.all-images').css({
-        'visibility': 'visible'
-      });
-      $('.all-images a').colorbox({
-        photo: true,
-        scalePhotos: true,
-        maxHeight: '90%',
-        maxWidth: '90%'
-      });
-    }
+  $(".show-images").unbind('click').on('click',function() {
+    let href = $(this).attr('href');
+    let modalImagesId = href.replace('#',"");
+    
+    let countRows = Math.ceil($(`#${modalImagesId}`).find('div.all-images>img').length / layoutNumber);
+    let layoutStr = new Array(countRows).fill(layoutNumber).join("");
+    $(`#${modalImagesId}`).find('div.all-images').photosetGrid({
+      highresLinks: true,
+      rel: 'withhearts-gallery',
+      gutter: '2px',
+      layout: layoutStr,
+      onComplete: function() {
+        $(`#${modalImagesId}`).find('.all-images').css({
+          'visibility': 'visible'
+        });
+        $(`#${modalImagesId}`).find('.all-images a').colorbox({
+          photo: true,
+          scalePhotos: true,
+          maxHeight: '90%',
+          maxWidth: '90%'
+        });
+      }
+    });
   });
 }
 
@@ -148,6 +153,10 @@ function changeScreenChat(){
     $(".person").removeClass("active");
     $(this).find("li").addClass("active");
     $(this).tab("show");
+
+    // cau hinh thanh cuon right-side
+    let divId = $(this).find("li").data("chat"); 
+    nineScrollRight(divId);
   });
 }
 
@@ -160,7 +169,6 @@ $(document).ready(function() {
 
   // Cấu hình thanh cuộn
   nineScrollLeft();
-  nineScrollRight();
 
   // Bật emoji, tham số truyền vào là id của box nhập nội dung tin nhắn
   enableEmojioneArea("17071995");
